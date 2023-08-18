7c02c3919f76901e7975661fc9fd8a96d2e207d6
Hari-Hrk <hrk.harish87@gmail.com>
2023-08-18 20:42:15 +0530

    near by location search

--- foodonline_main/views.py
+++ foodonline_main/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from vendor.models import Vendor
+from django.db.models import Q
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D # ``D`` is a shortcut for 
+from django.contrib.gis.db.models.functions import Distance
+
+
+def get_or_set_current_location(request):
+    if 'lat' in request.session:
+        lat = request.session['lat']
+        lng = request.session['lng']
+        return lat,lng
+    elif 'lat' in request.GET:
+        lat = request.GET.get('lat')
+        lng = request.GET.get('lng')
+        request.session['lat'] = lat
+        request.session['lng'] = lng
+        return lat,lng
+    else:
+        return None
 
 def home(request):
-    vendor = Vendor.objects.filter(is_approved=True,user__is_active=True)[:5]
+    if 'lat' in request.GET:
+        lat = request.GET.get('lat')
+        lng = request.GET.get('lng')
+
+        pnt = GEOSGeometry('POINT(%s %s)' % (lng,lat))
+        vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt,D(km=1000))).annotate(distance=Distance('user_profile__location',pnt)).order_by('distance')
+
+        for v in vendors: # distance came from annotate function
+            v.kms = round(v.distance.km,1)
+            
+    else:
+        vendors = Vendor.objects.filter(is_approved=True,user__is_active=True)[:5]
     context = {
-        'vendors':vendor
+        'vendors':vendors
     }
     return render(request,"home.html",context)
